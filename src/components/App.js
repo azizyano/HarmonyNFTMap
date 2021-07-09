7d35a3703907b5369bcd2f79fbae8c2bcfd90ced
azizyano <ynoaziz@gmailcom>
2021-07-10 00:30:26 +0100

    fix image dislpay

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import Navbar from "./Navbar";
 import Main from "./Main";
 import flag from "../logos/target.png";
 import { Map, Draggable } from "pigeon-maps";
-import ipfs from './IPFSUploader';
+import ipfs from "./IPFSUploader";
 import Web3 from "web3";
 import HERC721 from "../abis/HRC721.json";
 import HRC721Crowdsale from "../abis/HRC721Crowdsale.json";
 import "./App.css";
 
-const BN = require('bn.js');
+const BN = require("bn.js");
 class App extends Component {
   /** !UPDATE **/
   async componentWillMount() {
@@ -32,7 +32,6 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    
     let accounts,
       network,
       balance,
@@ -42,8 +41,6 @@ class App extends Component {
       contract2,
       contract1_abi,
       contract2_abi,
-      contract1_address,
-      contract2_address,
       nftbalance,
       tokenURL;
     web3 = window.web3;
@@ -55,8 +52,8 @@ class App extends Component {
     if (networkData) {
       contract1_abi = HERC721.abi;
       contract2_abi = HRC721Crowdsale.abi;
-      contract1_address = "0x6d6302fB1b5c995977EDd1F52fE8f2499D83455B";
-      contract2_address = "0x37ee84592F334dA6C81080e33F372330346dEfd9";
+      const contract1_address = "0x6d6302fB1b5c995977EDd1F52fE8f2499D83455B";
+      const contract2_address = "0x37ee84592F334dA6C81080e33F372330346dEfd9";
       contract1 = new web3.eth.Contract(contract1_abi, contract1_address);
       contract2 = new web3.eth.Contract(contract2_abi, contract2_address);
       accounts = await web3.eth.getAccounts();
@@ -77,13 +74,7 @@ class App extends Component {
       this.setState({
         contract1: contract1,
         contract2: contract2,
-        contractAddress1: contract2_address,
-        contractAddress2: contract2_address,
       });
-      const fit = await contract2.methods.getUrl(3).call()
-      const resp = await fetch(fit);
-      const hh = resp.json()
-      console.log(hh)
       // Load Colors
       for (var i = 1; i <= totalItems; i++) {
         const tokenURL = await contract2.methods.getUrl(i - 1).call();
@@ -113,7 +104,12 @@ class App extends Component {
       }
     });
   }
-
+  async loadingMetadata(URL) {
+    const resp = await fetch(URL);
+    return resp.json().then((metadata) => {
+      return metadata;
+    });
+  }
   onChange(value) {
     this.setState({ amount: value });
   }
@@ -123,8 +119,8 @@ class App extends Component {
     this.state = {
       anchor: [30.394324, -9.561427],
       account: null,
-      limit: '0',
-      price: '0',
+      limit: "0",
+      price: "0",
       amount: null,
       balance: null,
       contract1: null,
@@ -142,7 +138,7 @@ class App extends Component {
       file: null,
       ipfsHash: null,
       ipfsHash2: null,
-      jsondata:null
+      jsondata: null,
     };
 
     this.setState = this.setState.bind(this);
@@ -150,76 +146,82 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleupload = this.handleupload.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handledata = this.handledata.bind(this)
+    this.handledata = this.handledata.bind(this);
   }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
   }
-  handleupload(event){
-      event.preventDefault()
-      const file = event.target.files[0]
-      const reader = new window.FileReader()
-      reader.readAsArrayBuffer(file)
-      reader.onloadend = () => {
-        this.setState({ buffer: Buffer(reader.result) })
-        console.log('buffer', this.state.buffer)
-      }
+  handleupload(event) {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log("buffer", this.state.buffer);
+    };
   }
-  handledata= async (event) => {
-    event.preventDefault()
-    const jsondata= {
+  handledata = async (event) => {
+    event.preventDefault();
+    const jsondata = {
       ipfsHash: this.state.ipfsHash,
-      account : this.state.account,
-      Name : this.state.name,
-      limit : this.state.limit,
-      price : this.state.price,
-      coords : this.state.anchor[0]+', '+ this.state.anchor[1],
-    }
+      account: this.state.account,
+      Name: this.state.name,
+      limit: this.state.limit,
+      price: this.state.price,
+      coords: this.state.anchor[0] + ", " + this.state.anchor[1],
+    };
     var buf = Buffer.from(JSON.stringify(jsondata));
-    console.log(buf)
-    this.setState({jsondata: jsondata})
-    await ipfs.files.add(  { path: 'metadata.json', content: buf }, 
-    { wrapWithDirectory: true }, (error, result) => {
-      console.log(result[0].hash)
-      if(error) {
-        console.error(error)
-        return
+    console.log(buf);
+    this.setState({ jsondata: jsondata });
+    await ipfs.files.add(
+      { path: "metadata.json", content: buf },
+      { wrapWithDirectory: true },
+      (error, result) => {
+        console.log(result[0].hash);
+        if (error) {
+          console.error(error);
+          return;
+        }
+        return this.setState({ ipfsHash2: result[0].hash });
       }
-      return this.setState({ ipfsHash2: result[0].hash })
-    })
-    const gasPrice = new BN(await this.state.web3.eth.getGasPrice()).mul(new BN(1));
+    );
+    const gasPrice = new BN(await this.state.web3.eth.getGasPrice()).mul(
+      new BN(1)
+    );
     const gasLimit = 6721900;
-    const url = 'https://ipfs.io/ipfs/'+ this.state.ipfsHash2
-    this.state.contract2.methods.addItem(this.state.limit, this.state.price, url).send({ from: this.state.account, gasPrice, gasLimit })
-    .once('receipt', (receipt) => {
-      this.setState({
-        tokenURL: [...this.state.tokenURL, url]
-      })
-    })
-    this.state.contract1.methods.mint(this.state.account, this.state.totalItems).send({ from: this.state.account, gasPrice, gasLimit })
-    .once('receipt', (receipt) => {
-      this.setState({
-        tokenURL: [...this.state.tokenURL, url]
-      })
-    })
-    
-  }
+    const url = "https://ipfs.io/ipfs/" + this.state.ipfsHash2;
+    this.state.contract2.methods
+      .addItem(this.state.limit, this.state.price, url)
+      .send({ from: this.state.account, gasPrice, gasLimit })
+      .once("receipt", (receipt) => {
+        this.setState({
+          tokenURL: [...this.state.tokenURL, url],
+        });
+      });
+    this.state.contract1.methods
+      .mint(this.state.account, this.state.totalItems)
+      .send({ from: this.state.account, gasPrice, gasLimit })
+      .once("receipt", (receipt) => {
+        this.setState({
+          tokenURL: [...this.state.tokenURL, url],
+        });
+      });
+  };
 
-  handleSubmit= async  (event) => {
-    event.preventDefault()
+  handleSubmit = async (event) => {
+    event.preventDefault();
     await ipfs.files.add(this.state.buffer, (error, result) => {
-      console.log(result[0].hash)
-      if(error) {
-        console.error(error)
-        return
+      console.log(result[0].hash);
+      if (error) {
+        console.error(error);
+        return;
       }
-      return this.setState({ ipfsHash: result[0].hash })
-      
-    })
-    
-  }
+      return this.setState({ ipfsHash: result[0].hash });
+    });
+  };
 
   render() {
     return (
@@ -249,107 +251,147 @@ class App extends Component {
             web3={this.state.web3}
           />
         )}
-        <div className="container-fluid mt-5 col-m-4" style={{ maxWidth: '1000px' }}>
-        <div className="col-sm">
-          <main role="main" className="col-lg-12 text-monospace text-center text-white">
-            <div className="content mr-auto ml-auto">
-              <div id="content" className="mt-3" >
-                <div className="card mb-4 bg-dark border-danger">
-                  <div>
-        <div className="row text-center">
-          {this.state.tokenURL.map((tokenURL, key) => {
-            return (
-              <div key={key} className="col-md-3 mb-3">
-                <div
-                  className="token"
-                  style={{ backgroundColor: "blue" }}
-                ></div>
-                <div>{tokenURL}</div>
-                <img src={tokenURL} className="rounded img-fluid" alt="Cinque Terre" height="150px" width="150px"></img>
-              </div>
-            );
-          })}
-        </div>
-        <div className="container-fluid mt-5 text-monospace text-center mr-auto ml-auto">
-          <div>
-            <Map
-              height={500}
-              defaultCenter={[30.394324, -9.561427]}
-              defaultZoom={14}
+        <div
+          className="container-fluid mt-5 col-m-4"
+          style={{ maxWidth: "1000px" }}
+        >
+          <h5 className="card-title">list of all NFT minted:</h5>
+          <div className="col-sm">
+            <main
+              role="main"
+              className="col-lg-12 text-monospace text-center text-white"
             >
-              <Draggable
-                offset={[60, 87]}
-                anchor={this.state.anchor}
-                onDragEnd={(anchor) => this.setState({ anchor })}
-              >
-                <img src={flag} width={100} height={95} alt="Pigeon!" />
-              </Draggable>
-            </Map>
-          </div>
-        </div>
-        
-        <form onSubmit={this.handleSubmit}>
-        <div>
-            <label >Account: </label>
-           {this.state.account}
-          </div>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              name="name"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </div>
-          
-          <div>
-            <label htmlFor="numbres">limit Number of NFT</label>
-            <input
-              name="limit"
-              placeholder="limit"
-              value={this.state.limit}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="numbres">Price</label>
-            <input
-              name="price"
-              placeholder="price"
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="Coordinates">Coordinates: </label>
-            {' longitude: '+
-                this.state.anchor[0].toFixed(6) +
-                ", latitude: " +
-                this.state.anchor[1].toFixed(6)
-              }
-          </div>
+              <div className="content mr-auto ml-auto">
+                <div id="content" className="mt-3">
+                  <div className="card mb-4 bg-dark border-danger">
+                    <div>
+                      <div className="row text-center">
+                        {this.state.tokenURL.map((tokenURL, key) => {
+                          return (
+                            <div key={key} className="col-md-3 mb-3">
+                              <div
+                                className="token"
+                                style={{ backgroundColor: "blue" }}
+                              ></div>
+                              <div className="card-body card bg-info mb-3">
+                                <h5 className="card-title">NFT title </h5>
+                                <p className="card-text">
+                                  IPFS LINK {tokenURL}
+                                </p>
 
-        <div>
-        <input name="file" type="file" onChange={this.handleupload}/>
-        </div>
-        {!this.state.ipfsHash ? <div id="loader" className="spinner-border text-light" role="status"></div> :
-        <div>
-        <img src={'https://ipfs.io/ipfs/'+ this.state.ipfsHash} height="300" width="300"  alt=""/>
-        </div>}
-        <div>
-            <button>Upload Data</button>
-        </div>
-        </form>
-        <button onClick={this.handledata}>Create nft</button>
-        
-        </div>
+                                <img
+                                  src={tokenURL}
+                                  className="rounded img-fluid"
+                                  alt="Cinque Terre"
+                                  height="150px"
+                                  width="150px"
+                                ></img>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                      <div className="container-fluid mt-5 text-monospace text-center mr-auto ml-auto">
+                        <div>
+                          <Map
+                            height={500}
+                            defaultCenter={[30.394324, -9.561427]}
+                            defaultZoom={14}
+                          >
+                            <Draggable
+                              offset={[60, 87]}
+                              anchor={this.state.anchor}
+                              onDragEnd={(anchor) => this.setState({ anchor })}
+                            >
+                              <img
+                                src={flag}
+                                width={100}
+                                height={95}
+                                alt="Pigeon!"
+                              />
+                            </Draggable>
+                          </Map>
+                        </div>
+                      </div>
+
+                      <form onSubmit={this.handleSubmit}>
+                        <div>
+                          <label>Account: </label>
+                          {this.state.account}
+                        </div>
+                        <div>
+                          <label htmlFor="name">Name</label>
+                          <input
+                            name="name"
+                            placeholder="Name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+
+                        <div>
+                          <label htmlFor="numbres">limit Number of NFT</label>
+                          <input
+                            name="limit"
+                            placeholder="limit"
+                            value={this.state.limit}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <div>
+                          <label htmlFor="numbres">Price</label>
+                          <input
+                            name="price"
+                            placeholder="price"
+                            value={this.state.price}
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <div>
+                          <label htmlFor="Coordinates">Coordinates: </label>
+                          {" longitude: " +
+                            this.state.anchor[0].toFixed(6) +
+                            ", latitude: " +
+                            this.state.anchor[1].toFixed(6)}
+                        </div>
+
+                        <div>
+                          <input
+                            name="file"
+                            type="file"
+                            onChange={this.handleupload}
+                          />
+                        </div>
+                        {!this.state.ipfsHash ? (
+                          <div
+                            id="loader"
+                            className="spinner-grow text-light"
+                            role="status"
+                          ></div>
+                        ) : (
+                          <div>
+                            <img
+                              src={
+                                "https://ipfs.io/ipfs/" + this.state.ipfsHash
+                              }
+                              height="300"
+                              width="300"
+                              alt=""
+                            />
+                          </div>
+                        )}
+                        <div>
+                          <button>Upload Data</button>
+                        </div>
+                      </form>
+                      <button onClick={this.handledata}>Create nft</button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </main>
+            </main>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import Maplogo from '../logos/MapsLoga.png';
+import React, { Component } from "react";
 
 class Navbar extends Component {
-
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
+      <nav className="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow text-monospace">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-2"
           href="https://gitcoin.co/azizyano"
           target="_blank"
           rel="noopener noreferrer"
         >
-        <img src={Maplogo} height="32" alt="logo" />
-          NFT Map 
+          NFT Map
         </a>
-        {!this.props.account ? <div id="loader" className="spinner-border text-light" role="status"></div> :
+        {!this.props.account ? (
+          <div
+            id="loader"
+            className="spinner-border text-light"
+            role="status"
+          ></div>
+        ) : (
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <a
               className="text-white"
@@ -23,10 +26,11 @@ class Navbar extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              {(this.props.account)}
-            </a>&nbsp;
+              {this.props.account}
+            </a>
+            &nbsp;
           </li>
-        }
+        )}
       </nav>
     );
   }
